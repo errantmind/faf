a0f7cf5a398713a4717a87c67dca895e99586f61
James Bates <errantmind@protonmail.com>
2021-12-25 22:15:00 -0600

    separate constant for request / response buffer size into two, add docs

--- src/const_config.rs
+++ src/const_config.rs
@@ -47,8 +47,11 @@ macro_rules! const_concat_bytes {
 // faf spawns one thread per core, meaning each thread can handle 1024 connections
 pub const MAX_CONN: usize = 1024;
 
-// the buffer size of both the request and response buffers. Currently set to 4096 bytes (most common page size)
-pub const REQ_RES_BUFF_SIZE: usize = 4096;
+// the buffer size of the request buffer. Currently set to 4096 bytes (most common page size)
+pub const REQ_BUFF_SIZE: usize = 4096;
+
+// the buffer size of both the response buffers. Currently set to 4096 bytes (most common page size)
+pub const RES_BUFF_SIZE: usize = 4096;
 
 // our syscall to wait for epoll events will timeout every 1ms. This is marginally faster in some cases than a longer timeout
 pub const EPOLL_TIMEOUT_MILLIS: isize = 1;
--- src/epoll.rs
+++ src/epoll.rs
@@ -99,18 +99,18 @@ fn threaded_worker(port: u16, cb: fn(*const u8, usize, *const u8, usize, *mut u8
    let mut saved_event: epoll_event = unsafe { core::mem::zeroed() };
    saved_event.events = EPOLLIN;
 
-   let mut reqbuf: [u8; REQ_RES_BUFF_SIZE * MAX_CONN] = unsafe { core::mem::zeroed() };
-   let mut reqbuf_cur_addr: [isize; MAX_CONN] = unsafe { core::mem::zeroed() };
+   let mut reqbuf: [u8; REQ_BUFF_SIZE * MAX_CONN] = unsafe { core::mem::zeroed() };
 
-   // Init request buffer state
+   // Init state for tracking request buffer position across events
+   let mut reqbuf_cur_addr: [isize; MAX_CONN] = unsafe { core::mem::zeroed() };
    {
       let reqbuf_start_address = &mut reqbuf[0] as *mut _ as isize;
       (0..reqbuf_cur_addr.len()).for_each(|i| {
-         reqbuf_cur_addr[i] = reqbuf_start_address + i as isize * REQ_RES_BUFF_SIZE as isize;
+         reqbuf_cur_addr[i] = reqbuf_start_address + i as isize * REQ_BUFF_SIZE as isize;
       });
    }
 
-   let mut resbuf: [u8; REQ_RES_BUFF_SIZE] = unsafe { core::mem::zeroed() };
+   let mut resbuf: [u8; RES_BUFF_SIZE] = unsafe { core::mem::zeroed() };
    let resbuf_start_address = &mut resbuf[0] as *mut _ as isize;
 
    loop {
@@ -124,16 +124,23 @@ fn threaded_worker(port: u16, cb: fn(*const u8, usize, *const u8, usize, *mut u8
 
       for index in 0..num_incoming_events {
          let cur_fd = unsafe { (*epoll_events.get_unchecked(index as usize)).data.fd } as isize;
-         let req_buf_start_address = (&mut reqbuf[0] as *const u8 as isize).add(cur_fd * REQ_RES_BUFF_SIZE as isize);
+         let req_buf_start_address = (&mut reqbuf[0] as *const u8 as isize).add(cur_fd * REQ_BUFF_SIZE as isize);
+         let req_buf_cur_position = unsafe { reqbuf_cur_addr.get_unchecked_mut(cur_fd as usize) };
 
          if cur_fd == listener_fd {
+            if unlikely(cur_fd >= MAX_CONN as isize) {
+               sys_call!(SYS_CLOSE as isize, cur_fd);
+               continue;
+            }
+
             let incoming_fd = sys_call!(SYS_ACCEPT as isize, listener_fd as isize, 0, 0);
 
             if incoming_fd >= 0 {
+               *req_buf_cur_position = req_buf_start_address;
                net::setup_connection(incoming_fd, cpu_core as i32);
                saved_event.data.fd = incoming_fd as i32;
 
-               let _epoll_add_ret = sys_call!(
+               sys_call!(
                   SYS_EPOLL_CTL as isize,
                   epfd,
                   EPOLL_CTL_ADD as isize,
@@ -142,11 +149,10 @@ fn threaded_worker(port: u16, cb: fn(*const u8, usize, *const u8, usize, *mut u8
                );
             }
          } else {
-            let cur_address = unsafe { reqbuf_cur_addr.get_unchecked_mut(cur_fd as usize) };
-            let buffer_used = *cur_address - req_buf_start_address;
-            let buffer_remaining = REQ_RES_BUFF_SIZE as isize - buffer_used;
+            let buffer_used = *req_buf_cur_position - req_buf_start_address;
+            let buffer_remaining = REQ_BUFF_SIZE as isize - buffer_used;
 
-            let read = sys_call!(SYS_READ as isize, cur_fd, *cur_address, buffer_remaining);
+            let read = sys_call!(SYS_READ as isize, cur_fd, *req_buf_cur_position, buffer_remaining);
 
             if likely(read > 0) {
                let mut request_buffer_offset = 0;
@@ -190,13 +196,13 @@ fn threaded_worker(port: u16, cb: fn(*const u8, usize, *const u8, usize, *mut u8
                }
 
                if request_buffer_offset == 0 || response_buffer_filled_total == 0 {
-                  *cur_address = req_buf_start_address;
+                  *req_buf_cur_position = req_buf_start_address;
                   net::close_connection(epfd, cur_fd as isize);
                   continue;
                } else if request_buffer_offset == (read + buffer_used) {
-                  *cur_address = req_buf_start_address;
+                  *req_buf_cur_position = req_buf_start_address;
                } else {
-                  *cur_address += read;
+                  *req_buf_cur_position += read;
                }
 
                let wrote = sys_call!(
@@ -209,18 +215,18 @@ fn threaded_worker(port: u16, cb: fn(*const u8, usize, *const u8, usize, *mut u8
                if likely(wrote == response_buffer_filled_total) {
                } else if unlikely(-wrote == EAGAIN as isize || -wrote == EINTR as isize) {
                   {
-                     *cur_address = req_buf_start_address;
+                     *req_buf_cur_position = req_buf_start_address;
                      net::close_connection(epfd, cur_fd as isize);
                      break;
                   }
                } else {
-                  *cur_address = req_buf_start_address;
+                  *req_buf_cur_position = req_buf_start_address;
                   net::close_connection(epfd, cur_fd as isize);
                   continue;
                }
             } else if unlikely(-read == EAGAIN as isize || -read == EINTR as isize) {
             } else {
-               *cur_address = req_buf_start_address;
+               *req_buf_cur_position = req_buf_start_address;
                net::close_connection(epfd, cur_fd as isize);
             }
          }
